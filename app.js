2d054778e8445332f02b006bd37d7bae5e73f94f
Leah Miller <leahmiller1969@gmail.com>
2021-03-27 14:52:38 -0700

    bodyparser deprecated - html tags need name to parsed by express using urlencoded

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+
+app.use(express.urlencoded({extended:true}));
 
 app.use(express.static("src"));
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/signup.html");
-})
+});
+
+app.post("/", function(req,res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  console.log(req.body)
+  res.send('posted');
+});
 
 app.listen(3000, function(req,res){
   console.log('served!')
